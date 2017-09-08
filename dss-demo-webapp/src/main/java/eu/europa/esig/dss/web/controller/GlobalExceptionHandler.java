e1bb14b8a61c2645062b3f87d652e7386088e8d9
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2017-09-08 12:37:06 +0200

    cutsom http status

--- dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/controller/GlobalExceptionHandler.java
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/controller/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -28,18 +29,19 @@ public class GlobalExceptionHandler {
 
 		logger.error("Unhandle exception occured : " + e.getMessage(), e);
 
-		return getMAV(req, e);
+		return getMAV(req, e, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(NoHandlerFoundException.class)
 	public ModelAndView handle(HttpServletRequest req, Exception e) {
-		return getMAV(req, e);
+		return getMAV(req, e, HttpStatus.NOT_FOUND);
 	}
 
-	private ModelAndView getMAV(HttpServletRequest req, Exception e) {
+	private ModelAndView getMAV(HttpServletRequest req, Exception e, HttpStatus status) {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("exception", e);
 		mav.addObject("url", req.getRequestURL());
+		mav.setStatus(status);
 		mav.setViewName(DEFAULT_ERROR_VIEW);
 		return mav;
 	}
