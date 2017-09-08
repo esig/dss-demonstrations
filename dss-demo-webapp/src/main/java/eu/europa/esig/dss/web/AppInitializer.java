e70541afd1ac4b8f4df176dfa73f372f5e406d9d
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2017-09-08 11:16:27 +0200

    DSS-1250 : DSS demo : allows to use secure cookies

--- dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/AppInitializer.java
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/AppInitializer.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletRegistration;
 
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
@@ -14,30 +15,33 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
-
 public class AppInitializer implements WebApplicationInitializer {
+
+	@Value("${cookie.secure}")
+	private boolean cookieSecure;
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		WebApplicationContext context = getContext(); 
+		WebApplicationContext context = getContext();
 		servletContext.addListener(new ContextLoaderListener(context));
 		servletContext.addFilter("characterEncodingFilter", new CharacterEncodingFilter("UTF-8"));
-		
+
 		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("Dispatcher", new DispatcherServlet(context));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("/*");
-		
+
 		CXFServlet cxf = new CXFServlet();
 		BusFactory.setDefaultBus(cxf.getBus());
 		ServletRegistration.Dynamic cxfServlet = servletContext.addServlet("CXFServlet", cxf);
 		cxfServlet.setLoadOnStartup(1);
 		cxfServlet.addMapping("/services/*");
-		
+
 		servletContext.addFilter("springSecurityFilterChain", new DelegatingFilterProxy("springSecurityFilterChain")).addMappingForUrlPatterns(null, false,
 				"/*");
-		
+
+		servletContext.getSessionCookieConfig().setSecure(cookieSecure);
 	}
-	
+
 	private AnnotationConfigWebApplicationContext getContext() {
 		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
 		context.setConfigLocations("eu.europa.esig.dss.web.config");
