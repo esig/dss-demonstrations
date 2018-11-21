8c86d074778da17b2c2ef1c128f11dd476949b41
Pierrick Vandenbroucke <pierrick.vandenbroucke@nowina.lu>
2018-11-21 16:11:47 +0100

    Fix spring security config

--- dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/config/WebSecurityConfig.java
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/config/WebSecurityConfig.java
@@ -1,11 +1,17 @@
 package eu.europa.esig.dss.web.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.HeaderWriter;
+import org.springframework.security.web.header.writers.DelegatingRequestMatcherHeaderWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter.XFrameOptionsMode;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -32,9 +38,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 				.and().formLogin().loginPage("/login").failureUrl("/login-error").defaultSuccessUrl("/admin");
 
-
 		// javadoc uses frames
-//		http.antMatcher("/apidocs/**").headers().frameOptions().sameOrigin();
+		http.headers().addHeaderWriter(javadocHeaderWriter());
+	}
+
+	@Bean
+	public HeaderWriter javadocHeaderWriter() {
+		final AntPathRequestMatcher javadocAntPathRequestMatcher = new AntPathRequestMatcher("/apidocs/**");
+		final HeaderWriter hw = new XFrameOptionsHeaderWriter(XFrameOptionsMode.SAMEORIGIN);
+		return new DelegatingRequestMatcherHeaderWriter(javadocAntPathRequestMatcher, hw);
 	}
 
 	@Override
