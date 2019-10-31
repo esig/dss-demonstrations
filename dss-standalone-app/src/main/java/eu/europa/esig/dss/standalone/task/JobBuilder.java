3192178a7d6d467d2f10ae88353d05caf1822a7a
rricardo90 <ricardo.ribeiro-ferreira@nowina.lu>
2019-10-31 14:09:03 +0100

    DSS-1848 : LOTL Loader

---
+++ dss-standalone-app/src/main/java/eu/europa/esig/dss/standalone/task/JobBuilder.java
@@ -0,0 +1,115 @@
+package eu.europa.esig.dss.standalone.task;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.europa.esig.dss.model.DSSException;
+import eu.europa.esig.dss.service.http.commons.CommonsDataLoader;
+import eu.europa.esig.dss.service.http.commons.FileCacheDataLoader;
+import eu.europa.esig.dss.spi.client.http.DSSFileLoader;
+import eu.europa.esig.dss.spi.client.http.IgnoreDataLoader;
+import eu.europa.esig.dss.spi.tsl.TrustedListsCertificateSource;
+import eu.europa.esig.dss.spi.x509.CertificateSource;
+import eu.europa.esig.dss.spi.x509.KeyStoreCertificateSource;
+import eu.europa.esig.dss.tsl.cache.CacheCleaner;
+import eu.europa.esig.dss.tsl.function.OfficialJournalSchemeInformationURI;
+import eu.europa.esig.dss.tsl.job.TLValidationJob;
+import eu.europa.esig.dss.tsl.source.LOTLSource;
+import eu.europa.esig.dss.tsl.sync.AcceptAllStrategy;
+
+public class JobBuilder {
+
+	private static final Logger LOG = LoggerFactory.getLogger(JobBuilder.class);
+
+	private final String LOTL_URL = "https://ec.europa.eu/tools/lotl/eu-lotl.xml";
+	private final String OJ_URL = "https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=uriserv:OJ.C_.2019.276.01.0001.01.ENG";
+
+	private TrustedListsCertificateSource tslCertificateSource;
+
+	public JobBuilder() {
+		tslCertificateSource = new TrustedListsCertificateSource();
+	}
+
+	public TLValidationJob job() {
+		TLValidationJob job = new TLValidationJob();
+		job.setOnlineDataLoader(onlineLoader());
+		job.setOfflineDataLoader(offlineLoader());
+		job.setTrustedListCertificateSource(tslCertificateSource);
+		job.setSynchronizationStrategy(new AcceptAllStrategy());
+		job.setCacheCleaner(cacheCleaner());
+
+		LOTLSource europeanLOTL = europeanLOTL();
+		job.setListOfTrustedListSources(europeanLOTL);
+
+		return job;
+	}
+
+	private DSSFileLoader onlineLoader() {
+		FileCacheDataLoader offlineFileLoader = new FileCacheDataLoader();
+		offlineFileLoader.setCacheExpirationTime(0);
+		offlineFileLoader.setDataLoader(dataLoader());
+		offlineFileLoader.setFileCacheDirectory(tlCacheDirectory());
+		return offlineFileLoader;
+	}
+
+	private DSSFileLoader offlineLoader() {
+		FileCacheDataLoader offlineFileLoader = new FileCacheDataLoader();
+		offlineFileLoader.setCacheExpirationTime(Long.MAX_VALUE);
+		offlineFileLoader.setDataLoader(new IgnoreDataLoader()); // do not download from Internet
+		offlineFileLoader.setFileCacheDirectory(tlCacheDirectory());
+		return offlineFileLoader;
+	}
+
+	private CommonsDataLoader dataLoader() {
+		return new CommonsDataLoader();
+	}
+
+	private LOTLSource europeanLOTL() {
+		LOTLSource lotlSource = new LOTLSource();
+		lotlSource.setUrl(LOTL_URL);
+		lotlSource.setCertificateSource(officialJournalContentKeyStore());
+		lotlSource.setSigningCertificatesAnnouncementPredicate(new OfficialJournalSchemeInformationURI(OJ_URL));
+		lotlSource.setPivotSupport(true);
+		return lotlSource;
+	}
+
+	private CacheCleaner cacheCleaner() {
+		CacheCleaner cacheCleaner = new CacheCleaner();
+		cacheCleaner.setCleanMemory(true);
+		cacheCleaner.setCleanFileSystem(true);
+		cacheCleaner.setDSSFileLoader(getDSSFileLoader());
+		return cacheCleaner;
+	}
+
+	private CertificateSource officialJournalContentKeyStore() {
+		try {
+			return new KeyStoreCertificateSource(new File("src/main/resources/keystore.p12"), "PKCS12", "dss-password");
+		} catch (IOException e) {
+			throw new DSSException("Unable to load the keystore", e);
+		}
+	}
+
+	private FileCacheDataLoader getDSSFileLoader() {
+		FileCacheDataLoader fileLoader = new FileCacheDataLoader();
+		fileLoader.setCacheExpirationTime(0);
+		fileLoader.setFileCacheDirectory(tlCacheDirectory());
+		return fileLoader;
+	}
+
+	private File tlCacheDirectory() {
+		File rootFolder = new File(System.getProperty("java.io.tmpdir"));
+		File tslCache = new File(rootFolder, "dss-tsl-loader");
+		if (tslCache.mkdirs()) {
+			LOG.info("TL Cache folder : {}", tslCache.getAbsolutePath());
+		}
+		return tslCache;
+	}
+
+	public TrustedListsCertificateSource getCertificateSources() {
+		return tslCertificateSource;
+	}
+
+}
