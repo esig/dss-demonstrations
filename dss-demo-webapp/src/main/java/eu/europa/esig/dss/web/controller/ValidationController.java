f50d55bcfc4921f3903ca96f7baafd46ae7f8969
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2020-01-17 13:24:27 +0100

    class changed

--- dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/controller/ValidationController.java
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/controller/ValidationController.java
@@ -42,7 +42,7 @@ import eu.europa.esig.dss.spi.DSSUtils;
 import eu.europa.esig.dss.utils.Utils;
 import eu.europa.esig.dss.validation.CertificateVerifier;
 import eu.europa.esig.dss.validation.SignedDocumentValidator;
-import eu.europa.esig.dss.validation.executor.SignatureProcessExecutor;
+import eu.europa.esig.dss.validation.executor.DocumentProcessExecutor;
 import eu.europa.esig.dss.validation.executor.ValidationLevel;
 import eu.europa.esig.dss.validation.reports.Reports;
 import eu.europa.esig.dss.web.WebAppUtils;
@@ -111,7 +111,7 @@ public class ValidationController extends AbstractValidationController {
 			logger.warn("The request locale is null! Use the default one : {}", locale);
 		}
 		
-		SignatureProcessExecutor processExecutor = documentValidator.getDefaultProcessExecutor();
+		DocumentProcessExecutor processExecutor = documentValidator.getDefaultProcessExecutor();
 		processExecutor.setLocale(locale);
 		documentValidator.setProcessExecutor(processExecutor);
 
