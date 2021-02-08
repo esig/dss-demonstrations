ac422647454c8f49fb3fcb8341e0da153fccb497
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2021-02-08 08:39:31 +0100

    Fix build  + font config

--- dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/service/FOPService.java
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/service/FOPService.java
@@ -2,6 +2,7 @@ package eu.europa.esig.dss.web.service;
 
 import eu.europa.esig.dss.detailedreport.DetailedReportFacade;
 import eu.europa.esig.dss.simplereport.SimpleReportFacade;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.fop.apps.FOUserAgent;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
@@ -9,6 +10,8 @@ import org.apache.fop.apps.FopFactoryBuilder;
 import org.apache.fop.apps.MimeConstants;
 import org.apache.fop.configuration.Configuration;
 import org.apache.fop.configuration.DefaultConfigurationBuilder;
+import org.apache.xmlgraphics.io.Resource;
+import org.apache.xmlgraphics.io.ResourceResolver;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +19,10 @@ import javax.annotation.PostConstruct;
 import javax.xml.transform.Result;
 import javax.xml.transform.sax.SAXResult;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
 
 
 @Component
@@ -31,7 +36,7 @@ public class FOPService {
 	@PostConstruct
 	public void init() throws Exception {
 
-		FopFactoryBuilder builder = new FopFactoryBuilder(new File(".").toURI());
+		FopFactoryBuilder builder = new FopFactoryBuilder(new File(".").toURI(), new ClasspathResolver());
 		builder.setAccessibility(true);
 
 		try (InputStream is = new ClassPathResource(FOP_CONFIG).getInputStream()) {
@@ -58,6 +63,21 @@ public class FOPService {
 		Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, os);
 		Result result = new SAXResult(fop.getDefaultHandler());
 		DetailedReportFacade.newFacade().generatePdfReport(detailedReport, result);
+	}
+
+	private static class ClasspathResolver implements ResourceResolver {
+
+		@Override
+		public Resource getResource(URI uri) throws IOException {
+			return new Resource(FOPService.class.getResourceAsStream("/fonts/" + FilenameUtils.getName(uri.toString())));
+		}
+
+		@Override
+		public OutputStream getOutputStream(URI uri) throws IOException {
+			return Thread.currentThread().getContextClassLoader().getResource(uri.toString()).openConnection()
+					.getOutputStream();
+		}
+
 	}
 
 }
