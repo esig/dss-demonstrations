b48d1fa5c8210de0f4cd97ca863282405ebfbbac
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2021-03-05 10:34:38 +0100

    DSS-2325 : added a button to turn on/off annotations

---
+++ dss-demo-webapp/src/main/webapp/scripts/jsValidationResult.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+    $('#detailed-report-card > .card-header').append($('#detailed-report-buttons'));
+    $('#detailed-report-buttons').addClass('inside-card-header');
+});
+
+$('#detailed-report-buttons .btn').click(function(e) {
+    e.stopPropagation();
+    if ($(this).hasClass("switch-annotations")) {
+        switchAnnotations($(this), e);
+    }
+});
+
+function switchAnnotations($this, e) {
+    if ($this.hasClass("show")) {
+        $('#detailed-report .constraint-text').addClass('d-none');
+        $('#detailed-report .card-body .constraint .constraint-text').removeClass('d-block');
+        $('#detailed-report .constraint-tooltip').removeClass('d-none');
+        $this.find('.icon').addClass('fa-commenting-o');
+        $this.find('.icon').removeClass('fa-comment-o');
+        $this.find('.label-show').removeClass('d-none');
+        $this.find('.label-hide').addClass('d-none');
+        $this.removeClass('show');
+    } else {
+        $('#detailed-report .constraint-text').removeClass('d-none');
+        $('#detailed-report .card-body .constraint .constraint-text').addClass('d-block');
+        $('#detailed-report .constraint-tooltip').addClass('d-none');
+        $this.find('.icon').removeClass('fa-commenting-o');
+        $this.find('.icon').addClass('fa-comment-o');
+        $this.find('.label-show').addClass('d-none');
+        $this.find('.label-hide').removeClass('d-none');
+        $this.addClass('show');
+    }
+}
