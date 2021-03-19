c5616c4501d86a2dbbcd2e5282f820a1ca3c734c
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2021-03-19 11:04:49 +0100

    DSS-2370 : Set-Cookie SameSite=strict attribute added

--- dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/config/WebSecurityConfig.java
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/config/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package eu.europa.esig.dss.web.config;
 
+import eu.europa.esig.dss.utils.Utils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,10 +14,20 @@ import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter.XFrameOptionsMode;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.MappedInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("${web.security.cookie.samesite}")
+	private String samesite;
 	
 	/** API urls (REST/SOAP webServices) */
 	private static final String[] API_URLS = new String[] {
@@ -45,8 +58,41 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new DelegatingRequestMatcherHeaderWriter(javadocAntPathRequestMatcher, hw);
 	}
 	
+	@Bean
 	public HeaderWriter serverEsigDSS() {
 		return new StaticHeadersWriter("Server", "ESIG-DSS");
+	}
+
+	@Bean
+	public MappedInterceptor cookiesInterceptor() {
+		return new MappedInterceptor(null, new CookiesHandlerInterceptor());
+	}
+
+	/**
+	 * The class is used to enrich "Set-Cookie" header with "SameSite=strict" value
+	 *
+	 * NOTE: Spring does not provide support of cookies handling out of the box
+	 *       and requires a Spring Session dependency for that.
+	 *       Here is a manual way of response headers configuration
+	 */
+	private final class CookiesHandlerInterceptor implements HandlerInterceptor {
+
+		/** The "SameSite" cookie parameter name */
+		private static final String SAMESITE_NAME = "SameSite";
+
+		@Override
+		public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+							   ModelAndView modelAndView) {
+			if (Utils.isStringNotEmpty(samesite)) {
+				Collection<String> setCookieHeaders = response.getHeaders(HttpHeaders.SET_COOKIE);
+				if (Utils.isCollectionNotEmpty(setCookieHeaders)) {
+					for (String header : setCookieHeaders) {
+						header = String.format("%s; %s=%s", header, SAMESITE_NAME, samesite);
+						response.setHeader(HttpHeaders.SET_COOKIE, header);
+					}
+				}
+			}
+		}
 	}
 
 }
