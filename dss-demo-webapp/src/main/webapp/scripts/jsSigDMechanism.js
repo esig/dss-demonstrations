d6101065d92f71e77d905fbf256937978dd9b2aa
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2021-07-02 16:48:50 +0200

    DSS-2473 : improve user experiance with signature creation form

--- dss-demo-webapp/src/main/webapp/scripts/jsSigDMechanism.js
+++ dss-demo-webapp/src/main/webapp/scripts/jsSigDMechanism.js
@@ -1,21 +1,29 @@
 $('[data-toggle="tooltip"]').tooltip();
-
-$('input[name="sigDMechanism"]:radio').attr("disabled", true);
-
 function isDetached() {
 	var value = $('input[name=signaturePackaging]:checked').val();
 	return 'DETACHED' == value;
 }
 
+function updateSigDMechanism(sigPackaging) {
+	$('input[name="sigDMechanism"]:radio').attr("disabled", true);
+	if ('DETACHED' == sigPackaging) {
+		$('input[name="sigDMechanism"]:radio').filter('[value="OBJECT_ID_BY_URI"]').attr("disabled", false);
+		$('input[name="sigDMechanism"]:radio').filter('[value="OBJECT_ID_BY_URI_HASH"]').attr("disabled", false);
+	}
+}
+
 $('input[name="signaturePackaging"]:radio').change(
         function() {
         	$('input[name="sigDMechanism"]:radio').prop("checked", false);
-
-        	var packaging = this.value;
-        	if ('DETACHED' == packaging) {
-        		$('input[name="sigDMechanism"]:radio').filter('[value="OBJECT_ID_BY_URI"]').attr("disabled", false);
-        		$('input[name="sigDMechanism"]:radio').filter('[value="OBJECT_ID_BY_URI_HASH"]').attr("disabled", false);
-        	} else {
-        		$('input[name="sigDMechanism"]:radio').attr("disabled", true);
-        	}
+    	    updateSigDMechanism(this.value);
         });
+
+$(document).ready(function() {
+    var checkedPackaging = $('input[name="signaturePackaging"]:checked');
+    if (checkedPackaging.length) {
+    	updateSigDMechanism(checkedPackaging.val());
+    } else {
+        $('input[name="sigDMechanism"]:radio').attr("disabled", true);
+        $('input[name="sigDMechanism"]:radio').prop("checked", false);
+    }
+});
--- dss-demo-webapp/src/main/webapp/scripts/jsSignatureLevel.js
+++ dss-demo-webapp/src/main/webapp/scripts/jsSignatureLevel.js
@@ -5,85 +5,127 @@ $('input[name="containerType"]:radio').filter('[value="none"]').attr('checked', 
 
 $('#selectSignatureLevel').empty();
 
-function isAsicContainer() {
-	var container = $('input[name=containerType]:checked').val();
-	return 'none' != container;
+function isAsicContainer(asicValue) {
+	return 'none' != asicValue;
 }
 
-$('input[name="containerType"]:radio').change(
-    function() {
-        var asicValue = this.value;
+function updateSignatureForm(asicValue) {
+    $('input[name="signatureForm"]:radio').attr("disabled", true);
 
-        $('input[name="signatureForm"]:radio').prop("checked", false);
-        $('input[name="signatureForm"]:radio').attr("disabled", true);
+    $('#selectSignatureLevel').empty();
 
+    if ('none' == asicValue) {
+        $("#formCAdES").attr("disabled", false);
+        $("#formPAdES").attr("disabled", false);
+        $("#formXAdES").attr("disabled", false);
+        $("#formJAdES").attr("disabled", false);
+
+    } else {
+        $("#formCAdES").attr("disabled", false);
+        $("#formXAdES").attr("disabled", false);
+    }
+
+    $('input[name="signatureForm"]:radio').each(function() {
+        if ($(this).attr('disabled')) {
+            $(this).prop("checked", false);
+        }
+    });
+}
+
+function updateSignaturePackaging(asicValue, signatureForm) {
+    if (isAsicContainer(asicValue)) {
         $('input[name="signaturePackaging"]:radio').prop("checked", false);
         $('input[name="signaturePackaging"]:radio').attr("disabled", true);
 
-        $('#selectSignatureLevel').empty();
-
-        if ('none' == asicValue) {
-            $("#formCAdES").attr("disabled", false);
-            $("#formPAdES").attr("disabled", false);
-            $("#formXAdES").attr("disabled", false);
-            $("#formJAdES").attr("disabled", false);
-
-        } else {
-            $("#formCAdES").attr("disabled", false);
-            $("#formXAdES").attr("disabled", false);
-
-            $("#signaturePackaging-DETACHED").attr("disabled", false);
-            $("#signaturePackaging-DETACHED").prop("checked", true);
-
-        }
-    });
-
-
-$('input[name="signatureForm"]:radio').change(
-    function() {
-
-        $('#selectSignatureLevel').empty();
-
-        var process = $('#process').val();
-
-        if (!isAsicContainer()) {
-            $('input[name="signaturePackaging"]:radio').attr("disabled", true).prop("checked", false);
-
-            $.ajax({
-                type : "GET",
-                url : "data/packagingsByForm?form=" + this.value,
-                dataType : "json",
-                error : function(msg) {
-                    alert("Error !: " + msg);
-                },
-                success : function(data) {
-                    $.each(data, function(idx) {
-                        $('#signaturePackaging-' + data[idx]).attr("disabled", false);
-                    });
-                    if (data.length == 1) {
-                        $('#signaturePackaging-' + data[0]).prop("checked", true);
-                    }
-                }
-            });
-        }
-
+        $("#signaturePackaging-DETACHED").attr("disabled", false);
+        $("#signaturePackaging-DETACHED").prop("checked", true);
+    } else {
+        var currentSignaturePackaging = $('input[name="signaturePackaging"]:checked');
+        $('input[name="signaturePackaging"]:radio').attr("disabled", true).prop("checked", false);
         $.ajax({
             type : "GET",
-            url : "data/levelsByForm?form=" + this.value+"&process="+process,
+            url : "data/packagingsByForm?form=" + signatureForm,
             dataType : "json",
             error : function(msg) {
                 alert("Error !: " + msg);
             },
             success : function(data) {
-                $.each(data, function(idx) {
-                    $('#selectSignatureLevel').append($('<option>', {
-                        value: data[idx],
-                        text: data[idx].replace(/_/g, "-")
-                    }));
-                });
+                if (data.length == 1) {
+                    $.each(data, function(idx) {
+                        $('#signaturePackaging-' + data[idx]).attr("disabled", false);
+                        $('#signaturePackaging-' + data[idx]).prop("checked", true);
+                    });
+                } else {
+                    $.each(data, function(idx) {
+                        var sigPackaging = $('#signaturePackaging-' + data[idx]);
+                        $('#signaturePackaging-' + data[idx]).attr("disabled", false);
+                        if (currentSignaturePackaging != null && currentSignaturePackaging.val() == sigPackaging.val()) {
+                            $('#signaturePackaging-' + data[idx]).prop("checked", true);
+                        }
+                    });
+                }
             }
         });
+    }
+}
 
+function updateSignatureLevel(signatureForm) {
+    $('#selectSignatureLevel').empty();
+
+    var process = $('#process').val();
+
+    $.ajax({
+        type : "GET",
+        url : "data/levelsByForm?form=" + signatureForm + "&process=" + process,
+        dataType : "json",
+        error : function(msg) {
+            alert("Error !: " + msg);
+        },
+        success : function(data) {
+            $.each(data, function(idx) {
+                $('#selectSignatureLevel').append($('<option>', {
+                    value: data[idx],
+                    text: data[idx].replace(/_/g, "-")
+                }));
+            });
+        }
+    });
+}
+
+$('input[name="containerType"]:radio').change(
+    function() {
+
+        updateSignatureForm(this.value);
+        var checkedSignatureForm = $('input[name="signatureForm"]:checked');
+        if (checkedSignatureForm.length) {
+            updateSignaturePackaging(this.value, checkedSignatureForm.val())
+            updateSignatureLevel(checkedSignatureForm.val());
+        } else {
+            $('#selectSignatureLevel').empty();
+        }
+
+    });
+
+$('input[name="signatureForm"]:radio').change(
+    function() {
+
+        var checkedContainerType = $('input[name="containerType"]:checked');
+        if (checkedContainerType.length) {
+            var asicValue = checkedContainerType.val();
+            updateSignaturePackaging(asicValue, this.value)
+        }
+        updateSignatureLevel(this.value);
         getDigestAlgorithms(this.value);
 
     });
+
+$(document).ready(function() {
+    var checkedContainerType = $('input[name="containerType"]:checked');
+    var checkedSignatureForm = $('input[name="signatureForm"]:checked');
+    if (checkedContainerType.length && checkedSignatureForm.length) {
+        updateSignaturePackaging(checkedContainerType.val(), checkedSignatureForm.val())
+    }
+    if (checkedSignatureForm.length) {
+        updateSignatureLevel(checkedSignatureForm.val());
+    }
+});
