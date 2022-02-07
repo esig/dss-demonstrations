5c63b7ccbc709099cd226f31141d456a4ba9e4dc
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2022-02-07 14:19:57 +0100

    add default values for proxy configuration + null check
    (cherry picked from commit 3311657bc66d8c9e931d9aa1194dd44db3a4414f)

--- dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/config/ProxyConfiguration.java
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/config/ProxyConfiguration.java
@@ -1,5 +1,6 @@
 package eu.europa.esig.dss.web.config;
 
+import eu.europa.esig.dss.utils.Utils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,34 +13,34 @@ import java.util.Collection;
 @Configuration
 public class ProxyConfiguration {
 
-	@Value("${proxy.http.enabled}")
+	@Value("${proxy.http.enabled:false}")
 	private boolean httpEnabled;
-	@Value("${proxy.http.host}")
+	@Value("${proxy.http.host:}")
 	private String httpHost;
-	@Value("${proxy.http.port}")
+	@Value("${proxy.http.port:-1}")
 	private int httpPort;
-	@Value("${proxy.http.scheme}")
+	@Value("${proxy.http.scheme:}")
 	private String httpScheme;
-	@Value("${proxy.http.user}")
+	@Value("${proxy.http.user:}")
 	private String httpUser;
-	@Value("${proxy.http.password}")
+	@Value("${proxy.http.password:}")
 	private String httpPassword;
-	@Value("${proxy.http.exclude}")
+	@Value("${proxy.http.exclude:}")
 	private Collection<String> httpExcludedHosts;
 
-	@Value("${proxy.https.enabled}")
+	@Value("${proxy.https.enabled:false}")
 	private boolean httpsEnabled;
-	@Value("${proxy.https.host}")
+	@Value("${proxy.https.host:}")
 	private String httpsHost;
-	@Value("${proxy.https.port}")
+	@Value("${proxy.https.port:-1}")
 	private int httpsPort;
-	@Value("${proxy.https.scheme}")
+	@Value("${proxy.https.scheme:}")
 	private String httpsScheme;
-	@Value("${proxy.https.user}")
+	@Value("${proxy.https.user:}")
 	private String httpsUser;
-	@Value("${proxy.https.password}")
+	@Value("${proxy.https.password:}")
 	private String httpsPassword;
-	@Value("${proxy.https.exclude}")
+	@Value("${proxy.https.exclude:}")
 	private Collection<String> httpsExcludedHosts;
 
 	@Bean
@@ -50,22 +51,46 @@ public class ProxyConfiguration {
 		ProxyConfig config = new ProxyConfig();
 		if (httpEnabled) {
 			ProxyProperties httpProperties = new ProxyProperties();
-			httpProperties.setHost(httpHost);
-			httpProperties.setPort(httpPort);
-			httpProperties.setScheme(httpScheme);
-			httpProperties.setUser(httpUser);
-			httpProperties.setPassword(httpPassword);
-			httpProperties.setExcludedHosts(httpExcludedHosts);
+			if (Utils.isStringNotEmpty(httpHost)) {
+				httpProperties.setHost(httpHost);
+			}
+			if (httpPort != -1) {
+				httpProperties.setPort(httpPort);
+			}
+			if (Utils.isStringNotEmpty(httpScheme)) {
+				httpProperties.setScheme(httpScheme);
+			}
+			if (Utils.isStringNotEmpty(httpUser)) {
+				httpProperties.setUser(httpUser);
+			}
+			if (Utils.isStringNotEmpty(httpPassword)) {
+				httpProperties.setPassword(httpPassword);
+			}
+			if (Utils.isCollectionNotEmpty(httpExcludedHosts)) {
+				httpProperties.setExcludedHosts(httpExcludedHosts);
+			}
 			config.setHttpProperties(httpProperties);
 		}
 		if (httpsEnabled) {
 			ProxyProperties httpsProperties = new ProxyProperties();
-			httpsProperties.setHost(httpsHost);
-			httpsProperties.setPort(httpsPort);
-			httpsProperties.setScheme(httpsScheme);
-			httpsProperties.setUser(httpsUser);
-			httpsProperties.setPassword(httpsPassword);
-			httpsProperties.setExcludedHosts(httpsExcludedHosts);
+			if (Utils.isStringNotEmpty(httpsHost)) {
+				httpsProperties.setHost(httpsHost);
+			}
+			if (httpsPort != -1) {
+				httpsProperties.setPort(httpsPort);
+			}
+			if (Utils.isStringNotEmpty(httpsScheme)) {
+				httpsProperties.setScheme(httpsScheme);
+			}
+			if (Utils.isStringNotEmpty(httpsUser)) {
+				httpsProperties.setUser(httpsUser);
+			}
+			if (Utils.isStringNotEmpty(httpsPassword)) {
+				httpsProperties.setPassword(httpsPassword);
+			}
+			if (Utils.isCollectionNotEmpty(httpsExcludedHosts)) {
+				httpsProperties.setExcludedHosts(httpsExcludedHosts);
+			}
 			config.setHttpsProperties(httpsProperties);
 		}
 		return config;
