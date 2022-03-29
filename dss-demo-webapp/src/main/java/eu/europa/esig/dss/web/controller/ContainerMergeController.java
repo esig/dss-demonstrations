b9592cb1041ad3c04fb8fbd8365e45396418f1a7
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2022-03-29 16:52:48 +0200

    DSS-2716 : Add a webpage with ASiC merger possibility

---
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/controller/ContainerMergeController.java
@@ -0,0 +1,84 @@
+package eu.europa.esig.dss.web.controller;
+
+import eu.europa.esig.dss.asic.common.merge.ASiCContainerMerger;
+import eu.europa.esig.dss.asic.common.merge.DefaultContainerMerger;
+import eu.europa.esig.dss.model.DSSDocument;
+import eu.europa.esig.dss.model.MimeType;
+import eu.europa.esig.dss.utils.Utils;
+import eu.europa.esig.dss.web.WebAppUtils;
+import eu.europa.esig.dss.web.model.ContainerMergeForm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+@Controller
+@SessionAttributes(value = { "mergeContainersForm", "mergedContainer" })
+@RequestMapping(value = "/merge-containers")
+public class ContainerMergeController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ContainerMergeController.class);
+
+    private static final String CONTAINERS_MERGE = "containers-merge";
+
+    private static final String[] ALLOWED_FIELDS = { "documentsToMerge" };
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder webDataBinder) {
+        webDataBinder.setAllowedFields(ALLOWED_FIELDS);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String display(Model model, HttpServletRequest request) {
+        ContainerMergeForm containerMergeForm = new ContainerMergeForm();
+        model.addAttribute("containerMergeForm", containerMergeForm);
+        return CONTAINERS_MERGE;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String mergeContainers(Model model, HttpServletResponse response,
+                                  @ModelAttribute("containerMergeForm") @Valid ContainerMergeForm containerMergeForm, BindingResult result) {
+        if (result.hasErrors()) {
+            if (LOG.isDebugEnabled()) {
+                List<ObjectError> allErrors = result.getAllErrors();
+                for (ObjectError error : allErrors) {
+                    LOG.debug(error.getDefaultMessage());
+                }
+            }
+            return CONTAINERS_MERGE;
+        }
+
+        List<DSSDocument> containers = WebAppUtils.toDSSDocuments(containerMergeForm.getDocumentsToMerge());
+        ASiCContainerMerger merger = DefaultContainerMerger.fromDocuments(containers.toArray(new DSSDocument[0]));
+        DSSDocument mergedContainer = merger.merge();
+
+        MimeType mimeType = mergedContainer.getMimeType();
+        if (mimeType != null) {
+            response.setContentType(mimeType.getMimeTypeString());
+        }
+        response.setHeader("Content-Transfer-Encoding", "binary");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + mergedContainer.getName() + "\"");
+        try (InputStream is = mergedContainer.openStream(); OutputStream os = response.getOutputStream()) {
+            Utils.copy(is, os);
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+}
---
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/model/ContainerMergeForm.java
@@ -0,0 +1,51 @@
+package eu.europa.esig.dss.web.model;
+
+import eu.europa.esig.dss.asic.common.ASiCUtils;
+import eu.europa.esig.dss.web.WebAppUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.AssertTrue;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class ContainerMergeForm {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ContainerMergeForm.class);
+
+    private List<MultipartFile> documentsToMerge;
+
+    public List<MultipartFile> getDocumentsToMerge() {
+        return documentsToMerge;
+    }
+
+    public void setDocumentsToMerge(List<MultipartFile> documentsToMerge) {
+        this.documentsToMerge = documentsToMerge;
+    }
+
+    @AssertTrue(message = "{error.to.merge.containers.mandatory}")
+    public boolean isDocumentsToMerge() {
+        return WebAppUtils.isCollectionNotEmpty(documentsToMerge);
+    }
+
+    @AssertTrue(message = "{error.to.merge.containers.valid.zip}")
+    public boolean isDocumentsToMergeValid() {
+        if (documentsToMerge != null) {
+            for (MultipartFile file : documentsToMerge) {
+                if (!file.isEmpty()) {
+                    try (InputStream is = file.getInputStream()) {
+                        if (!ASiCUtils.isZip(is)) {
+                            return false;
+                        }
+                    } catch (IOException e) {
+                        LOG.warn("Unable to read the file with name '{}'. Reason : {}", file.getName(), e.getMessage(), e);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+}
