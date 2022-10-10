e34e61e4d2ac5c35a730300a4da4d2ca284622b9
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2022-10-10 16:13:06 +0200

    make init method public
    (cherry picked from commit f27c2e2dfd93137f8f5b96347abf27be6698d84d)

--- dss-esig-validation-tests/src/test/java/eu/europa/esig/dss/validation/EsigValidationTest.java
+++ dss-esig-validation-tests/src/test/java/eu/europa/esig/dss/validation/EsigValidationTest.java
@@ -73,7 +73,7 @@ public class EsigValidationTest {
     private static StringBuilder sb;
 
     @BeforeAll
-    private static void init() throws Exception {
+    public static void init() throws Exception {
         ValidationPolicyFacade policyFacade = ValidationPolicyFacade.newFacade();
         ConstraintsParameters constraints = policyFacade.unmarshall(new File(POLICY_URL));
         validationPolicy = new EtsiValidationPolicy(constraints);
