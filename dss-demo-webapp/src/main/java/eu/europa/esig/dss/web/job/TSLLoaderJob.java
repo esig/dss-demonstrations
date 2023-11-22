3fff5a68a1189fe205852253f2f0fd21785034df
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2023-11-22 15:45:16 +0100

    fix TLValidationJob offline refresh

--- dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/job/TSLLoaderJob.java
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/job/TSLLoaderJob.java
@@ -1,12 +1,12 @@
 package eu.europa.esig.dss.web.job;
 
+import eu.europa.esig.dss.tsl.job.TLValidationJob;
+import eu.europa.esig.dss.utils.Utils;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import eu.europa.esig.dss.tsl.job.TLValidationJob;
 
 @Service
 public class TSLLoaderJob {
@@ -14,11 +14,17 @@ public class TSLLoaderJob {
 	@Value("${cron.tl.loader.enable}")
 	private boolean enable;
 
+	@Value("${bc.rsa.max_mr_tests:}")
+	private String bcRsaValidation;
+
 	@Autowired
 	private TLValidationJob job;
 
 	@PostConstruct
 	public void init() {
+		if (Utils.isStringNotEmpty(bcRsaValidation)) {
+			System.setProperty("org.bouncycastle.rsa.max_mr_tests", bcRsaValidation);
+		}
 		job.offlineRefresh();
 	}
 
