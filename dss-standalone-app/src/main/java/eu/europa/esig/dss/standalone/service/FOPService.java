429239b9aad20a99aeaccdc8ca1c56249c24406f
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2025-07-08 12:21:22 +0200

    DSS-3619 : fix simple/detailed report generation in DSS-standalone
    (cherry picked from commit 6686a746f9d1a16eaa54fd821b2a2a75e0a09d67)

--- dss-standalone-app/src/main/java/eu/europa/esig/dss/standalone/service/FOPService.java
+++ dss-standalone-app/src/main/java/eu/europa/esig/dss/standalone/service/FOPService.java
@@ -5,20 +5,25 @@ import eu.europa.esig.dss.detailedreport.jaxb.XmlDetailedReport;
 import eu.europa.esig.dss.simplereport.SimpleReportFacade;
 import eu.europa.esig.dss.simplereport.jaxb.XmlSimpleReport;
 import eu.europa.esig.dss.standalone.exception.ApplicationException;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.fop.apps.EnvironmentalProfileFactory;
 import org.apache.fop.apps.FOUserAgent;
 import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopConfParser;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.FopFactoryBuilder;
 import org.apache.fop.apps.MimeConstants;
-import org.apache.fop.configuration.Configuration;
-import org.apache.fop.configuration.DefaultConfigurationBuilder;
+import org.apache.xmlgraphics.io.Resource;
+import org.apache.xmlgraphics.io.ResourceResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.xml.transform.Result;
 import javax.xml.transform.sax.SAXResult;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
 
 public class FOPService {
 
@@ -37,12 +42,11 @@ public class FOPService {
 
     private void init() {
         try (InputStream is = getClass().getResourceAsStream(FOP_CONFIG)) {
-            FopFactoryBuilder builder = new FopFactoryBuilder(getClass().getResource("/").toURI());
-            builder.setAccessibility(true);
 
-            DefaultConfigurationBuilder configurationBuilder = new DefaultConfigurationBuilder();
-            Configuration configuration = configurationBuilder.build(is);
-            builder.setConfiguration(configuration);
+            FopFactoryBuilder builder = new FopConfParser(is,
+                    EnvironmentalProfileFactory.createRestrictedIO(new URI("file:/"), new ClasspathResolver())).getFopFactoryBuilder();
+
+            builder.setAccessibility(true);
 
             fopFactory = builder.build();
 
@@ -73,6 +77,21 @@ public class FOPService {
         Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, os);
         Result result = new SAXResult(fop.getDefaultHandler());
         DetailedReportFacade.newFacade().generatePdfReport(detailedReport, result);
+    }
+
+    private static class ClasspathResolver implements ResourceResolver {
+
+        @Override
+        public Resource getResource(URI uri) {
+            return new Resource(FOPService.class.getResourceAsStream("/fonts/" + FilenameUtils.getName(uri.toString())));
+        }
+
+        @Override
+        public OutputStream getOutputStream(URI uri) throws IOException {
+            return Thread.currentThread().getContextClassLoader().getResource(uri.toString()).openConnection()
+                    .getOutputStream();
+        }
+
     }
 
 }
